eda3710f6d55d99dbcd2fb9a2b83afbf6b28aae3
NetNinjaEngineer <memo5260287@outlook.com>
2024-04-02 13:07:31 +0200

    Validate Options Using DataAnnotations

--- GettingStartedWithConfigurationConcepts/Helpers/ExternalServicesConfiguration.cs
+++ GettingStartedWithConfigurationConcepts/Helpers/ExternalServicesConfiguration.cs
@@ -1,7 +1,10 @@
-﻿namespace GettingStartedWithConfigurationConcepts.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GettingStartedWithConfigurationConcepts.Helpers
 {
     public class ExternalServicesConfiguration
     {
+        [Required]
         public string? Url { get; set; }
 
         public int MinsToCache { get; set; }
--- GettingStartedWithConfigurationConcepts/Program.cs
+++ GettingStartedWithConfigurationConcepts/Program.cs
@@ -9,9 +9,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
 #region Using Named Options with IOptionsSnapshot and IOptionsMonitor
-builder.Services.Configure<ExternalServicesConfiguration>(Constants.WeatherApi, builder.Configuration.GetSection("ExternalServices:WeatherApi"));
-builder.Services.Configure<ExternalServicesConfiguration>(Constants.WeatherApi, builder.Configuration.GetSection("ExternalServices:ProductsApi"));
+//builder.Services.Configure<ExternalServicesConfiguration>(Constants.WeatherApi, builder.Configuration.GetSection("ExternalServices:WeatherApi"));
+builder.Services.Configure<ExternalServicesConfiguration>(Constants.ProductsApi, builder.Configuration.GetSection("ExternalServices:ProductsApi"));
 
+#endregion
+
+#region Validate Options Using Data Annotations
+builder.Services.AddOptions<ExternalServicesConfiguration>()
+    .Bind(builder.Configuration.GetSection("ExternalServices:WeatherApi"))
+    .ValidateDataAnnotations();
 #endregion
 
 builder.Services.AddHttpClient();
